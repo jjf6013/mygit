2d7cbe3473672fb64c4e569df8765781ef693b57
Jaa <jjf6013@foxmail.com>
2022-06-15 00:27:33 +0800

    third commit

--- src/main/java/com/jjf/Git_Test.java
+++ src/main/java/com/jjf/Git_Test.java
@@ -4,5 +4,6 @@ public class Git_Test {
     public static void main(String[] args) {
         System.out.println("hello Git");
         System.out.println("hello Git1");
+        System.out.println("hello Git2");
     }
 }
